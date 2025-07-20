98dfc97b763f3a6121f7f770277cb7c35004fd4b
0dgq1 <96348905+0dgq1@users.noreply.github.com>
2025-07-20 16:31:11 +0800

    实现管理员的订单管理和退票管理功能

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import AdminLayout from '../components/AdminLayout'
 import AdminProtectedRoute from '../components/AdminProtectedRoute'
 import { AdminDashboard, UserManagement, OrderManagement } from '../pages/admin'
+import RefundManagement from '../pages/admin/RefundManagement'
 
 const AdminRoutes: React.FC = () => {
   return (
@@ -13,7 +14,7 @@ const AdminRoutes: React.FC = () => {
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/users" element={<UserManagement />} />
           <Route path="/admin/orders" element={<OrderManagement />} />
-          <Route path="/admin/refunds" element={<div>退票管理功能开发中...</div>} />
+          <Route path="/admin/refunds" element={<RefundManagement />} />
           <Route path="/admin/spots" element={<div>景点管理功能开发中...</div>} />
           <Route path="/admin/reports" element={<div>报表统计功能开发中...</div>} />
         </Routes>
